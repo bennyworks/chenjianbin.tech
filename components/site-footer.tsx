40e2bff9c007c9beb82c50807c3022bba2856af6
陈剑彬 <chenjianbin@mingxinsk.com>
2024-12-10 11:37:04 +0800

    refactor: 添加格式化配置和修改网站的图标

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,18 +1,18 @@
-import * as React from "react"
+import * as React from 'react'
 
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
-import { Icons } from "@/components/icons"
-import { ModeToggle } from "@/components/mode-toggle"
+import { siteConfig } from '@/config/site'
+import { cn } from '@/lib/utils'
+import { Icons } from '@/components/icons'
+import { ModeToggle } from '@/components/mode-toggle'
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Icons.logo />
+          <Icons.chenjianbin />
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by{" "}
+            Built by{' '}
             <a
               href={siteConfig.links.twitter}
               target="_blank"
@@ -21,7 +21,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
             >
               shadcn
             </a>
-            . Hosted on{" "}
+            . Hosted on{' '}
             <a
               href="https://vercel.com"
               target="_blank"
@@ -30,7 +30,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
             >
               Vercel
             </a>
-            . Illustrations by{" "}
+            . Illustrations by{' '}
             <a
               href="https://popsy.co"
               target="_blank"
@@ -39,7 +39,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
             >
               Popsy
             </a>
-            . The source code is available on{" "}
+            . The source code is available on{' '}
             <a
               href={siteConfig.links.github}
               target="_blank"
