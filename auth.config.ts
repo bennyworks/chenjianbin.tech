a89c40e3d16affaf722f05f931d9f9c8ae8a6220
陈剑彬 <bennyworks@foxmail.com>
2025-05-24 19:40:32 +0800

    feat: 配置 GitHub OAuth 认证并添加登录页面路由控制

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,29 @@
 import GitHub from 'next-auth/providers/github'
 import type { NextAuthConfig } from 'next-auth'
 
-export default { providers: [GitHub] } satisfies NextAuthConfig
+export default {
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
+      if (isOnDashboard) {
+        if (isLoggedIn) return true
+        return false // 重定向到登录页面
+      }
+      
+      if (isLoggedIn) {
+        return true
+      }
+      return true
+    },
+  },
+  pages: {
+    signIn: '/login',
+  },
+} satisfies NextAuthConfig
