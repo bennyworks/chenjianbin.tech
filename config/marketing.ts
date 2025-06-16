b78cc132791b4afbaa442295f91c1ab17167108a
陈剑彬 <bennyworks@foxmail.com>
2025-06-16 16:47:53 +0800

    简化网站首页，移除产品展示、角色展示和号召区部分

--- config/marketing.ts
+++ config/marketing.ts
@@ -3,26 +3,6 @@ import type { MarketingConfig } from 'types'
 export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
-      title: '产品',
-      href: '/#products',
-    },
-    {
-      title: 'Data-Pulse',
-      href: 'https://data-pulse.chenjianbin.tech',
-    },
-    {
-      title: 'Biz-Pulse',
-      href: 'https://biz-pulse.chenjianbin.tech',
-    },
-    {
-      title: '解决方案',
-      href: '/solutions',
-    },
-    {
-      title: '价格',
-      href: '/pricing',
-    },
-    {
       title: '文档',
       href: '/docs',
     },
