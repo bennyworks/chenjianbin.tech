c60fc93c077f9520aae0b493e1801b0d87373082
陈剑彬 <bennyworks@foxmail.com>
2025-06-18 18:31:40 +0800

    feat: 添加工具函数用于日期格式化、农历转换和样式合并

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,6 @@ import { twMerge } from 'tailwind-merge'
 
 import { env } from '@/env.mjs'
 import { Lunar } from 'lunar-javascript'
-import { EventFormData } from '@/types/event'
 
 export function getSolarToLunar(date: string): string {
   const d = new Date(date)
@@ -54,35 +53,4 @@ export function formatTime(input: Date): string {
 
 export function absoluteUrl(path: string) {
   return `${env.NEXT_PUBLIC_APP_URL}${path}`
-}
-
-export function getDefaultEventData(value?: number | string | Date): EventFormData {
-  if (!value) {
-    // 如果没有传入时间，默认为当前日期的08:00
-    value = new Date()
-  } else {
-    value =
-      new Date(value).toLocaleDateString('en-CA') + 'T' + new Date().toLocaleTimeString('zh-CN')
-  }
-  // 获取默认的事项数据
-  const startDateTime = new Date(value)
-  const endDateTime = new Date(startDateTime.getTime() + 60 * 60 * 1000)
-
-  const event: EventFormData = {
-    title: '',
-    memberId: '',
-    startDate: startDateTime.toLocaleDateString('en-CA'),
-    startTime: formatTime(startDateTime),
-    endDate: endDateTime.toLocaleDateString('en-CA'),
-    endTime: formatTime(endDateTime),
-    duration: '1', // 处理时长1小时
-    isAllDay: false, // 非全天
-    location: '', // 处理地点
-    description: '', // 描述
-    reminder: '15', // 提前15分钟提醒
-    repeat: 'NoRepeat', // 不重复，即不用按照一定的周期重复处理，
-    attachments: [], // 附件
-  }
-
-  return event
 }
