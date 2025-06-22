1850bc9c13ab545d798eab8e5edd07772200c2d5
陈剑彬 <bennyworks@foxmail.com>
2025-06-22 22:37:20 +0800

    feat: 更新tailwind配置，优化样式定义，调整全局样式以使用@apply语法

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -1,8 +1,17 @@
-'use client'
+"use client"
 
-import { BadgeCheck, Bell, LogOut, Sparkles } from 'lucide-react'
+import {
+  BadgeCheck,
+  Bell,
+  LogOut,
+  Sparkles,
+} from "lucide-react"
 
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,14 +20,14 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+} from "@/components/ui/dropdown-menu"
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
-} from '@/components/ui/sidebar'
-import { CaretSortIcon, ComponentPlaceholderIcon } from '@radix-ui/react-icons'
+} from "@/components/ui/sidebar"
+import { CaretSortIcon, ComponentPlaceholderIcon } from "@radix-ui/react-icons"
 import { signOut } from 'next-auth/react'
 
 export function NavUser({
@@ -54,7 +63,7 @@ export function NavUser({
           </DropdownMenuTrigger>
           <DropdownMenuContent
             className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-            side={isMobile ? 'bottom' : 'right'}
+            side={isMobile ? "bottom" : "right"}
             align="end"
             sideOffset={4}
           >
